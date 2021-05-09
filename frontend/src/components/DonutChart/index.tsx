5c46c3b1ab03d219a95df1358a167dabab7c8cb9
PomboObeso <matheus.gomes201@gmail.com>
2021-05-09 00:36:52 -0300

    Fixing Bug on DonutChart 2

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -31,12 +31,12 @@ const DonutChart = () => {
     //let chartData : ChartData = {labels:[],series: []};
 
 
-
+/*
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-
+*/
     const options = {
         legend: {
             show: true
