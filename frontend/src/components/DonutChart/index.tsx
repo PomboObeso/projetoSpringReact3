c8d4feb61202caf0c7dd8d21168e43c13f7a96b6
PomboObeso <matheus.gomes201@gmail.com>
2021-05-09 00:33:21 -0300

    Fixing Bug on DonutChart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -45,8 +45,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels, }}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels, }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
